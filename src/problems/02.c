77c18464c591c15e205ed041eead08e54f5a1ceb
Dylan Allbee <dylan@allbee.org>
2017-12-02 18:57:02 -0800

    Solve problem 1:a

--- src/problems/02.c
+++ src/problems/02.c
@@ -1,9 +1,32 @@
+#include <limits.h>
+#include <stdlib.h>
 #include <klib/kvec.h>
 #include <sds/sds.h>
 #include "../common.h"
 
+static int row_difference(sds row) {
+    int count;
+    sds *tokens = sdssplitargs(row, &count);
+
+    int small = INT_MAX;
+    int large = INT_MIN;
+    for (int i = 0; i < count; ++i) {
+        int value = atoi(tokens[i]);
+        small = min(small, value);
+        large = max(large, value);
+    }
+
+    sdsfreesplitres(tokens, count);
+    return large - small;
+}
+
 sds solve_02_a(sdsvec input) {
-    return sdsempty();
+    int checksum = 0;
+    for (size_t i = 0; i < kv_size(input); ++i) {
+        checksum += row_difference(kv_A(input, i));
+        printf("checksum: %d\n", checksum);
+    }
+    return sdsfromlonglong(checksum);
 }
 
 sds solve_02_b(sdsvec input) {
